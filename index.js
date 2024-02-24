f1c65f47b4322fe4a8e387d44c408d94444a5cc1
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-02-24 23:10:39 +0600

    a
    a

---
+++ index.js
@@ -0,0 +1,24 @@
+function arre()
+{
+    let array=[2,5,6,7,9,8,1,3];
+array.sort();
+
+// console.log(array);
+
+for(let aee of array){
+    console.log(aee);
+}
+}
+
+
+arre( ); 
+
+
+
+
+let stz={
+    name: "John",
+    age: 30,
+    work:"feni"
+};
+console.log(stz .name);
