8915f3416a9e0af897c0c2630fa4a50aece41b89
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-02-28 23:44:08 +0600

    r
    r

---
+++ evenloop.js
@@ -0,0 +1,13 @@
+console.log(1);
+
+setTimeout(function timeout() {
+    console.log ('mohiuddin')
+} 
+, 2000)
+console.log(2);
+console.log(3);
+// intervel men continus running
+// setInterval(function interval(){
+//    console.log('interval');
+// },5000)
+console.log(4);
